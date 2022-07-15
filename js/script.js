bb2b9bd50ec5c6ab86afc5c14adbdc4a89e4cee3
dvdnyll <109359894+dvdnyll@users.noreply.github.com>
2022-07-15 21:07:29 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,143 @@
+let nama, val;
+const url_string = document.URL;
+const url = new URL(url_string);
+let doi;
+
+if (url.searchParams.get('in') != null) {
+  doi = url.searchParams.get('in');
+} else {
+  doi = "Razellya Mungil";
+}
+
+let footer = document.getElementById("credit");
+footer.innerHTML = doi;
+footer.href = "your bf, Jakaver Gofzaren";
+
+function time() {
+  var d = new Date();
+  var n = d.getHours();
+  console.log(n);
+  if (n >= 5 && n <= 10) {
+    return "Pagi"
+  } else if (n >= 10 && n <= 15) {
+    return "Siang"
+  } else if (n >= 15 && n <= 18) {
+    return "Sore"
+  } else if (n >= 18 && n <= 24) {
+    return "Malam"
+  }
+}
+
+function makan() {
+  switch (time()) {
+    case "Pagi":
+      return "sarapan ya, I love you"
+      break;
+    case "Siang":
+      return "makan siang, jangan lupa. I still love you"
+      break;
+    case "Malam":
+      return "makan malam, jangan di skip. I love you so much"
+      break;
+      case "Sore":
+      return "time to break babyyyy"
+      break;
+    default:
+      break;
+  }
+}
+
+function selesai() {
+  const teks = document.getElementById('teks');
+  const btn = document.querySelector('.tombol');
+  teks.innerHTML = `Semangat ${doi} <i
+  class="fas fa-heart text-danger animate__animated animate__heartBeat animate__repeat-3"></i>`;
+  btn.classList.add('d-none');
+  teks.classList.remove('d-none');  
+}
+
+
+document.querySelector(".tombol").addEventListener('click', function () {
+  Swal.fire({
+    title: 'Hai byy!',
+    html: `Selamat ${time()}, Kamu udah ${makan()}?`,
+    timer: 2500,
+    timerProgressBar: true,
+    showConfirmButton: false,
+  }).then((result) => {
+    if (result.dismiss === Swal.DismissReason.timer) {
+      Swal.fire('Kalau belum jangan lupa makan yaa').then((result) => {
+        Swal.fire({
+          title: ' ',
+          html: `Hidup di dunia emang susahh banget`,
+          timer: 2000,
+          timerProgressBar: true,
+          showConfirmButton: false,
+        }).then((result) => {
+          if (result.dismiss === Swal.DismissReason.timer) {
+            Swal.fire({
+              title: ' ',
+              html: `Tapi jangan berhenti buat bertahan`,
+              timer: 2000,
+              timerProgressBar: true,
+              showConfirmButton: false,
+            }).then((result) => {
+              if (result.dismiss === Swal.DismissReason.timer) {
+                Swal.fire('Semangat terus ya kamu :)').then((result) => {
+                  Swal.fire(
+                    'Jangan keseringan begadang!',
+                    '',
+                    'error'
+                  ).then((result) => {
+                    Swal.fire(
+                      'Jangan telat makan!',
+                      '',
+                      'error'
+                    ).then((result) => {
+                      Swal.fire('Aku dukung kamu terus ko').then((result) => {
+                        Swal.fire({
+                          title: 'Oh iya aku mau nanya',
+                          text: 'Ada sesuatu yang lagi kamu rasain gak??',
+                          showDenyButton: true,
+                          confirmButtonText: `Ada`,
+                          denyButtonText: `Ga ada ko`,
+                        }).then((result) => {
+                          /* Read more about isConfirmed, isDenied below */
+                          if (result.isConfirmed) {
+                            Swal.fire({
+                              title: 'Kenapa?',
+                              input: 'text',
+                              inputPlaceholder: 'bilang aja',
+                              showCancelButton: false,
+                              inputValidator: (value) => {
+                                if (!value) {
+                                  return 'Isi dulu hehhhh anak kecil'
+                                }
+                              }
+                            }).then((result) => {
+                              Swal.fire('Okey ay', 'This system was saved your mind as secret', 'success').then((result) => {
+                                Swal.fire("Your boy doesnt know your secret").then((result) => {
+                                  selesai()
+                                });
+                              })
+                            })
+                          } else if (result.isDenied) {
+                            Swal.fire('Oh okedeh kalo ga ada').then((result) => {
+                              Swal.fire("Love You").then((result) => {
+                                selesai()
+                              });
+                            })
+                          }
+                        })
+                      })
+                    })
+                  })
+                })
+              }
+            })
+          }
+        })
+      })
+    }
+  })
+});
